7e63783318c04c038f161b9bf48595534ba2ee4b
Rafael Oliveira <roliveira@iieducacao.com.br>
2025-09-03 18:34:50 -0300

    concluido

--- ps2_input.sv
+++ ps2_input.sv
@@ -1,25 +1,21 @@
 module ps2_input (
-    input  logic clk,       // clock do sistema (ex: 50 MHz)
+    input  logic clk,     
     input  logic ps2_clk,
     input  logic ps2_data,
     output reg acoes [0:5]
 );
 
-    // Flip-flops de sincronização para ps2_clk e ps2_data
     logic ps2_clk_sync_0, ps2_clk_sync_1;
     logic ps2_data_sync_0, ps2_data_sync_1;
 
-    // Detecta borda negativa no ps2_clk sincronizado
     logic ps2_clk_sync_prev;
 
-    // Registro para capturar os bits do dado
     reg [10:0] key_data;
     reg [3:0] bit_count;
     reg break_code;
 	 
 	 logic [7:0] scan_code;
     
-    // Sincronização de ps2_clk
     always_ff @(posedge clk) begin
         ps2_clk_sync_0 <= ps2_clk;
         ps2_clk_sync_1 <= ps2_clk_sync_0;
@@ -30,16 +26,14 @@ module ps2_input (
         ps2_clk_sync_prev <= ps2_clk_sync_1;
     end
 
-    // Captura dados na borda negativa do ps2_clk sincronizado
     always_ff @(posedge clk) begin
-        if (ps2_clk_sync_prev && !ps2_clk_sync_1) begin  // borda negativa detectada
+        if (ps2_clk_sync_prev && !ps2_clk_sync_1) begin 
             key_data[bit_count] <= ps2_data_sync_1;
             bit_count <= bit_count + 1;
 
             if (bit_count == 10) begin
                 bit_count <= 0;
 
-                // Processa scan_code e ações aqui
                 scan_code = key_data[8:1];
 
                 if (scan_code == 8'hF0) begin
